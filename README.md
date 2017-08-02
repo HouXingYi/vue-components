bb827f8ab64404af937c1abdfed692f821de26df
houxingyi <1026043046@qq.com>
2017-08-02 20:21:15 +0800

    change README

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 
 # components
-积累一些平常使用的常见的vue组件
+积累一些平常使用的常见的vue组件(进行中)
 ## How to start
 1. git clone 
 2. npm install
--- src/components/Index/Index.vue
+++ src/components/Index/Index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="hello">
+  <div class="index">
     <span>{{msg}}</span>
   </div>
 </template>
